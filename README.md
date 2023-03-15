3c3c0c52f66e3398afa8ede8ba9d0e860ff7ed84
unknown <ti@joaoti.com.br>
2023-03-15 14:50:14 -0300

    start of the project

---
+++ README.md
@@ -0,0 +1,1 @@
+"# autenticacao-jwt-nodejs" 
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => console.log("Server started on port 3000"));
